e8b1781855262a4021a71f87e2bba988f14880ff
Makova Nataliya <116758787+Crot2050@users.noreply.github.com>
2023-04-04 21:13:36 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 
-valid_email = 'nikolaev.nebo@yandex.ru'
-valid_password = 'du!v4DU8yqKcFQj'
-invalid_email = 'qwerty@mail.ru'
-invalid_password = 'dsfedgreghre45543'
-correct_phone = '+7 999 900-51-09'
-incorrect_phone = '+7 985 911-01-09'
+valid_email = 'nataliya1175@mail.ru'
+valid_password = 'wPC-pVW-MK5-mvZ'
+invalid_email = 'none@mail.ru'
+invalid_password = 'password'
+correct_phone = '+79536101999'
+incorrect_phone = '+79998887766'
