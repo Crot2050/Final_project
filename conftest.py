fe73bb1794120ef165591ec0b481fd98a01da7a0
Makova Nataliya <116758787+Crot2050@users.noreply.github.com>
2023-04-04 21:12:59 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,8 +5,6 @@ import uuid
 
 @pytest.fixture
 def chrome_options(chrome_options):
-    # chrome_options.binary_location = '/usr/bin/google-chrome-stable'
-    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--log-level=DEBUG')
     return chrome_options
@@ -14,8 +12,6 @@ def chrome_options(chrome_options):
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
 def pytest_runtest_makereport(item, call):
-    # This function helps to detect that some test failed
-    # and pass this information to teardown:
 
     outcome = yield
     rep = outcome.get_result()
@@ -29,20 +25,15 @@ def web_browser(request, selenium):
     browser = selenium
     browser.set_window_size(1400, 1000)
 
-    # Return browser instance to test case:
     yield browser
 
-    # Do tear down (this code will be executed after each test):
-
     if request.node.rep_call.failed:
-        # Make the screenshot if test failed:
+
         try:
             browser.execute_script("document.body.bgColor = 'white';")
 
-            # Make screenshot for local debug:
             browser.save_screenshot('screenshots/' + str(uuid.uuid4()) + '.png')
 
-            # Attach screenshot to Allure report:
             allure.attach(browser.get_screenshot_as_png(),
                           name=request.function.__name__,
                           attachment_type=allure.attachment_type.PNG)
@@ -54,13 +45,10 @@ def web_browser(request, selenium):
                 print(log)
 
         except:
-            pass  # just ignore any errors here
+            pass
 
 
 def get_test_case_docstring(item):
-    """ This function gets doc string from test case and format it
-        to show this docstring instead of the test case name in reports.
-    """
 
     full_name = ''
 
@@ -84,25 +72,16 @@ def get_test_case_docstring(item):
 
 
 def pytest_itemcollected(item):
-    """ This function modifies names of test cases "on the fly"
-        during the execution of test cases.
-    """
 
     if item._obj.__doc__:
         item._nodeid = get_test_case_docstring(item)
 
 
 def pytest_collection_finish(session):
-    """ This function modified names of test cases "on the fly"
-        when we are using --collect-only parameter for pytest
-        (to get the full list of all existing test cases).
-    """
 
     if session.config.option.collectonly is True:
         for item in session.items:
-            # If test case has a doc string we need to modify its name to
-            # its doc string to show human-readable reports and to
-            # automatically import test cases to test management system.
+
             if item._obj.__doc__:
                 full_name = get_test_case_docstring(item)
                 print(full_name)
